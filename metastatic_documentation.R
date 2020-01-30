8bc1f458cc732509ae1b96da99098e5095842073
Michael Skaro <mfs61958@uga.edu>
2020-01-30 14:38:04 -0500

    All cancers hit. Now to customize each csv

--- metastatic_documentation.R
+++ metastatic_documentation.R
@@ -244,18 +244,63 @@ for(i in projects){
     
     
     PRAD_met <- as.data.frame(dat %>%
-                                dplyr::select(bcr_patient_barcode,new_tumor_event_after_initial_treatment,
-                                              new_neoplasm_event_type,
-                                              additional_surgery_metastatic_procedure,
-                                              additional_surgery_locoregional_procedure,
-                                              other_malignancy_anatomic_site,location_in_lung_parenchyma,
-                                              `new_neoplasm_event_type[1]`,`new_neoplasm_event_type[2]`,
-                                              malignancy_type,other_malignancy_histological_type,other_malignancy_histological_type_text))
+                                dplyr::select(bcr_patient_barcode,new_tumor_event_after_initial_treatment,new_tumor_event_after_initial_treatment,
+                                              bone_scan_results,new_neoplasm_event_type,new_neoplasm_event_occurrence_anatomic_site,malignancy_type,
+                                              other_malignancy_anatomic_site,histological_type_other,tumor_progression_post_ht,other_malignancy_anatomic_site_text,
+                                              other_malignancy_histological_type, other_malignancy_histological_type_text,new_neoplasm_occurrence_anatomic_site_text
+                                              ))
     
     write.csv(PRAD_met, file = str_glue("~/storage/PanCancerAnalysis/TCGABiolinks/metastatic_clin_info/{i}_metastatic_staus_.csv"))
     
     print("PRAD Done")
     rm(PRAD_met)
+    
+    
+    
+  } 
+  
+  if(i== "TCGA-STAD"){
+    
+    dat <- as.data.frame(data.table::fread(str_glue("~/CSBL_shared/clinical/TCGA_xml/{i}.csv"))) 
+    
+    
+    STAD_met <- as.data.frame(dat %>%
+                                dplyr::select(bcr_patient_barcode,number_of_lymphnodes_positive_by_he,
+                                              new_tumor_event_after_initial_treatment,new_neoplasm_event_type,
+                                              new_neoplasm_event_occurrence_anatomic_site,additional_surgery_metastatic_procedure,
+                                              malignancy_type,other_malignancy_anatomic_site,other_malignancy_histological_type,
+                                              other_malignancy_histological_type_text,additional_surgery_locoregional_procedure,
+                                              new_neoplasm_occurrence_anatomic_site_text,days_to_additional_surgery_locoregional_procedure,
+                                              residual_disease_post_new_tumor_event_margin_status,`new_neoplasm_event_type[1]`,`new_neoplasm_event_type[2]`,
+                                              other_malignancy_anatomic_site_text))
+    
+    write.csv(PRAD_met, file = str_glue("~/storage/PanCancerAnalysis/TCGABiolinks/metastatic_clin_info/{i}_metastatic_staus_.csv"))
+    
+    print("STAD Done")
+    rm(STAD_met)
+    
+    
+    
+  } 
+  
+  if(i== "TCGA-THCA"){
+    
+    dat <- as.data.frame(data.table::fread(str_glue("~/CSBL_shared/clinical/TCGA_xml/{i}.csv"))) 
+    
+    
+    THCA_met <- as.data.frame(dat %>%
+                                dplyr::select(bcr_patient_barcode,number_of_lymphnodes_positive_by_he,lymph_node_preoperative_assessment_diagnostic_imaging_type,
+                                              new_tumor_event_after_initial_treatment,new_neoplasm_event_type,
+                                              new_neoplasm_event_occurrence_anatomic_site,metastatic_site, other_metastatic_site,
+                                              malignancy_type,other_malignancy_anatomic_site,other_malignancy_anatomic_site_text,
+                                              other_malignancy_histological_type,new_neoplasm_event_type,new_tumor_event_additional_surgery_procedure,
+                                              new_neoplasm_event_occurrence_anatomic_site,other_malignancy_histological_type_text,metastatic_neoplasm_confirmed_diagnosis_method_name,
+                                              `metastatic_neoplasm_confirmed_diagnosis_method_name[1]`,`metastatic_neoplasm_confirmed_diagnosis_method_name[2]`))
+    
+    write.csv(PRAD_met, file = str_glue("~/storage/PanCancerAnalysis/TCGABiolinks/metastatic_clin_info/{i}_metastatic_staus_.csv"))
+    
+    print("THCA Done")
+    rm(THCA_met)
     
     
     
