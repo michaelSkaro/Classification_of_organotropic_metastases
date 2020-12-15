411997c50d03e28034e68f7053df9b10fce17537
Marcus <marcdh@uga.edu>
2020-12-15 14:06:47 -0500

    Added requirements to setup

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ import setuptools
 setuptools.setup(
     name='mot',
     version='1.0',
-    packages=['mot'],
+    packages=['mot', 'mot.demo'],
     package_dir={
-        'mot':'src/'
+        'mot':'src/',
+        'mot.demo':'src/demo'
     },
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'seaborn',
+        'scikit-learn',
+        'tqdm',
+    ]
 )
